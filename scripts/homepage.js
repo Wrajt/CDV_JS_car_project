37d779f747a2481551fa2d3000e782383b08988f
Wrajt <kgrabarek59@gmail.com>
2024-03-17 08:47:43 +0100

    #3 -> więcej samochodów, częściowy json, uzupełnienie grafik, małe zmiany w stylowaniu

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,14 +1,16 @@
 const cars = [
     {
-        "name": "Lola",
+        "name": "Ramone",
         "brand": "Chevrolet",
-        "model": "Bel Air",
-        "image": "assets/lola.png",
-        "price": `83 900$`,
-        "year": 1957,
-        "power": 162,
-        "mileage": 113300
-    },{
+        "model": "Impala",
+        "image": "assets/ramone1.png",
+        "price": "57 950$",
+        "year": 1959,
+        "power": "185kw",
+        "mileage": "85 171 km"
+    },
+
+    {
         "name": "Lighting McQueen",
         "brand": "Ferrari",
         "model": "599 GTB Fiorano",
@@ -27,8 +29,83 @@ const cars = [
         "year": 1957,
         "power": "unknown",
         "mileage": "12 000 km"
+    },
+    {
+        "name": "Sally Carrera",
+        "brand": "Porsche",
+        "model": "Carrera (996)",
+        "image": "assets/sally.png",
+        "price": "39 000$",
+        "year": 2002,
+        "power": "320kw",
+        "mileage": "99 000 km"
+    },
+    {
+        "name": "Snot Rod",
+        "brand": "Dodge",
+        "model": "Charger",
+        "image": "assets/snotrod.png",
+        "price": "101 000$",
+        "year": 1969,
+        "power": "368kw",
+        "mileage": "55 726 km"
+    },
+    {
+        "name": "Chick Hicks",
+        "brand": "Shyster",
+        "model": "Cremlin",
+        "image": "assets/chickhicks.png",
+        "price": "113 000$",
+        "year": 1979,
+        "power": "400kw",
+        "mileage": "28 726 km"
+    },
+    {
+        "name": "Jackson Storm",
+        "brand": "Hudson",
+        "model": "Hornet",
+        "image": "assets/jacksonstorm.png",
+        "price": "83 900",
+        "year": 1957,
+        "power": "162kw",
+        "mileage": "113 300 km"
+    },
+    {
+        "name": "Max Schnell",
+        "brand": "Mercedes-Benz",
+        "model": "AMG C-Class (W204) DTM",
+        "image": "assets/maxschnell.png",
+        "price": "120 000$",
+        "year": 2009,
+        "power": "470kw",
+        "mileage": "90 000 km"
+    },
+    {
+        "name": "Lewis Hamilton",
+        "brand": "McLaren",
+        "model": "MP4 12C",
+        "image": "assets/lewishamilton.png",
+        "price": "180 000$",
+        "year": 2009,
+        "power": "575kw",
+        "mileage": "20 000 km"
+    },
+    {
+        "name": "Holley Shiftwell",
+        "brand": "McLaren",
+        "model": "MP4 12C",
+        "image": "assets/holley.png",
+        "price": "180 000$",
+        "year": 2008,
+        "power": "275kw",
+        "mileage": "20 000 km"
     }
 ]
+
+
+
+
+
 
 // Function to create a car element
 function createCarElement(car) {
