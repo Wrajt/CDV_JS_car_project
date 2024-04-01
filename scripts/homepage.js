945393de68cfb6f42c468ceeada478778cf1efa7
Wrajt <kgrabarek59@gmail.com>
2024-04-01 22:01:48 +0200

    #5 -> complete configuration form #7 -> accessories

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,4 @@
 import cars from './cars.js';
-
 // Function to create a car element
 function createCarElement(car) {
     const carDiv = document.createElement("div");
@@ -34,7 +33,7 @@ function createCarElement(car) {
     }
 
     const configureLink = document.createElement("a");
-    configureLink.classList.add("button");
+    configureLink.classList.add("button", "config");
     configureLink.href = "#";
     const span = document.createElement("span");
     span.textContent = "Configure model";
@@ -44,7 +43,6 @@ function createCarElement(car) {
     return carDiv;
 }
 // Function to display details of the selected car and additional options
-// Function to display details of the selected car
 function displaySelectedCar(car) {
     // Create a new div for displaying selected car details
     const selectedCarDiv = document.createElement("div");
@@ -68,16 +66,17 @@ function displaySelectedCar(car) {
     for (const property in car) {
         if (car.hasOwnProperty(property) && property !== 'name' && property !== 'brand' && property !== 'model' && property !== 'image') {
             const listItem = document.createElement("li");
+            listItem.classList.add(property);
             listItem.textContent = `${property.charAt(0).toUpperCase() + property.slice(1)}: ${car[property]}`;
             carDetailList.appendChild(listItem);
         }
     }
 
     const backLink = document.createElement("a");
-    backLink.classList.add("back");
+    backLink.classList.add("button","back");
     backLink.href = "homepage.html";
     const span = document.createElement("span");
-    span.textContent = "Powrót";
+    span.textContent = " ← Back";
     backLink.appendChild(span);
 
 
@@ -88,8 +87,9 @@ function displaySelectedCar(car) {
     selectedCarDiv.appendChild(carDetailList);
     selectedCarDiv.appendChild(backLink);
 
-    // Append the selected car div to the document body
-    document.body.appendChild(selectedCarDiv);
+    const settingsContainer = document.querySelector(".settings-container");
+    settingsContainer.appendChild(selectedCarDiv);
+
 }
 
 // Function to handle button click event
@@ -101,6 +101,8 @@ function handleConfigureButtonClick(event) {
     // Hide the cars container
     const carsContainer = document.querySelector(".cars-container");
     carsContainer.style.display = "none";
+    const settingsContainer = document.querySelector(".settings-container");
+    settingsContainer.style.display = "flex";
 
     // Get the selected car index
     const selectedCarIndex = parseInt(carDiv.dataset.index);
@@ -109,8 +111,6 @@ function handleConfigureButtonClick(event) {
     // Display the selected car details
     displaySelectedCar(selectedCar);
 }
-
-
 // Function to add car elements to the HTML
 function addCarsToHTML(carsArray) {
     const carContainer = document.querySelector(".cars-container");
@@ -125,14 +125,48 @@ function addCarsToHTML(carsArray) {
 document.addEventListener("DOMContentLoaded", function() {
     addCarsToHTML(cars);
 
-    const buttons = document.querySelectorAll('.button');
+    const buttons = document.querySelectorAll('.config');
     buttons.forEach(button => {
         button.addEventListener('click', handleConfigureButtonClick);
     });
 });
 
+//ERROR MESSAGE FOR CAR FORM
+document.getElementById("car-form").addEventListener("submit", function(event) {
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let pickup = document.getElementById("selectDate").value;
+
+    if (name === '' || surname === '' || pickup === '') {
+      document.getElementById("error-msg").style.display = "block";
+      event.preventDefault();
+    }
+});
+
+//FUNCTION FOR PICKUP DATE
+function generateDate() {
+    let selectElement = document.getElementById("selectDate");
+    let selectedValue = parseInt(selectElement.value);
+    let currentDate = new Date();
+    let nextDate = new Date();
+    nextDate.setDate(currentDate.getDate() + selectedValue);
+    let formattedDate = nextDate.toLocaleDateString("en-US");
+    let datesContainer = document.getElementById("dates");
+    datesContainer.innerHTML = "<p>Your pick up date: " + formattedDate + "</p>";
+}
+
+window.onload = function() {
+    generateDate();
+};
+
+document.getElementById("selectDate").addEventListener("change", generateDate);
+
+//FUNCTION FOR PRICE CALCULATION
+function calculateTotalPrice(){
+let priceField = document.querySelector(".price").innerText;
+let carPrice = priceField.split(":")[1].trim();
 
 
-
-
-
+let totalField = document.querySelector(".total");
+totalField.innerHTML = "Your total is: " + carPrice;
+console.log(carPrice);}
